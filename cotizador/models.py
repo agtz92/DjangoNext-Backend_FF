d5631a65a878024932845b109d529e75b7c3827f
agtz92 <alfredo.gtz92@gmail.com>
2025-02-04 22:58:24 -0600

    dark mode

--- cotizador/models.py
+++ cotizador/models.py
@@ -4,11 +4,18 @@ import uuid
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    email = models.EmailField(
+        blank=False,
+        max_length=254,
+        verbose_name="email address"
+    )
+    dark_mode = models.BooleanField(
+        default=False,
+        verbose_name="Dark Mode"
+    )
 
-    email = models.EmailField(blank=False, max_length=254, verbose_name="email address")
-
-    USERNAME_FIELD = "username"   # e.g: "username", "email"
-    EMAIL_FIELD = "email"         # e.g: "email", "primary_email"
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
 
 class Company(models.Model):
     name = models.CharField(max_length=255, unique=True)
